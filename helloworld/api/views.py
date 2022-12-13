e0d076284b448b158f80be61c6f0a9634d1c7fb4
Harry Winer <winer.harry@gmail.com>
2022-12-13 15:13:07 +0000

    added Class based request routing with sample post request

--- helloworld/api/views.py
+++ helloworld/api/views.py
@@ -1,7 +1,21 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 
 @api_view(['GET'])
 def getData(request):
     person = {'name':"John", 'age': 21}
     return Response(person)
+
+@api_view(['POST'])
+def postData(request):
+    print("POST data: ", request.data)
+    return Response("Thank you for POSTing")
+
+class HelloWorld(APIView):
+    def get(self, request):
+        person = {'name':"Harry", 'age': 21}
+        return Response(person)
+    def post(self, request):
+        print("POST data: ", request.data)
+        return Response("Thank you for POSTing")
